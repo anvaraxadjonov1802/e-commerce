b2caa59f0b55ebd4fa86dd21495a0f647c90724e
Ubuntu <ubuntu@ip-172-31-75-138.ec2.internal>
2026-03-12 11:56:11 +0000

    4th commit from ubuntu

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from .serializers import OrderCreateSerializer, OrderSerializer
 from .models import Order
 
